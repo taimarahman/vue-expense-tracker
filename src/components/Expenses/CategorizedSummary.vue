2b11454f66f8caa7d6e516883c9f34e4e1e4eeed
Taima Rahman <taimarh@gmail.com>
2024-07-25 00:43:48 +0600

    feat: add categorized summary component

---
+++ src/components/Expenses/CategorizedSummary.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+<template>
+<div class="space-y-6">
+    <p class="text-xl text-dark font-medium pb-2 border-b-2 border-b-primary-300">Categorized Summary</p>
+    <div class="space-y-3">
+        <div class="flex-between p-3 rounded text-primary-600 bg-white shadow-1 ">
+            <p>Shopping</p>
+            <p>$400</p>
+        </div>
+        <div class="flex-between p-3 rounded text-white bg-primary-600 shadow-1 ">
+            <p>Bills</p>
+            <p>$700</p>
+        </div>
+        <div class="flex-between p-3 rounded text-dark bg-primary-300 shadow-1 ">
+            <p>Medicine</p>
+            <p>$245</p>
+        </div>
+        <div class="flex-between p-3 rounded text-primary-600 bg-white shadow-1 ">
+            <p>Food</p>
+            <p>$377</p>
+        </div>
+    </div>
+</div>
+</template>
--- src/views/pages/ExpenseView.vue
+++ src/views/pages/ExpenseView.vue
@@ -1,5 +1,12 @@
 <script setup>
+import CategorizedSummary from "../../components/Expenses/CategorizedSummary.vue"
 </script>
 <template>
-        <p>this is Expense page</p>
+    <div class="page-setup">
+        <div class="grid grid-cols-3 gap-x-3">
+            <div class="bg-red">cds</div>
+            <div class="bg-blue-300">dsc</div>
+            <CategorizedSummary/>
+        </div>
+    </div>
 </template>
